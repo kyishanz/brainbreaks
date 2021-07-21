48420a9feef2d780e697e0d2ee959c31f3724124
Lelani Do <lelaniado@gmail.com>
2021-07-21 13:46:23 -0700

    added second page

--- brainbreaks/ViewController.swift
+++ brainbreaks/ViewController.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var rightArrow: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
-        button.layer.cornerRadius = 25.8
-        button.tintColor = UIColor.white
+       
     }
     
 
