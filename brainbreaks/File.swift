d04fa9d555e8e377f5fe78bcab1cef5d27815d4d
Lelani Do <lelaniado@gmail.com>
2021-07-22 12:00:55 -0700

    added eye breaks slide, working on to do list

---
+++ brainbreaks/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  brainbreaks
+//
+//  Created by Lelani Do on 7/22/21.
+//
+
+import Foundation
---
+++ brainbreaks/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  brainbreaks
+//
+//  Created by Lelani Do on 7/22/21.
+//
+
+import UIKit
+
+class ToDo {
+   var name = ""
+}
+
+var newToDo = ToDo()
+print(newToDo)
